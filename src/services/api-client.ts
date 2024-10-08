8f0970437c48c7d1447310f2e3eed2b106879ebe
Emanuela <83732293+desmavi@users.noreply.github.com>
2024-10-08 20:07:49 +0200

    fix

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = 'https://apiexpress-h11yn8mvq-emanuelas-projects-04ae6b4b.vercel.app/'
+const BASE_URL = 'https://apiexpress-h11yn8mvq-emanuelas-projects-04ae6b4b.vercel.app'
 
 const api = axios.create({
     baseURL: BASE_URL,
